d2e6c388ab3dda403cdd8e1eb40b91a2f4e0289a
Ing-yeo_Developer <sjw7324@naver.com>
2022-11-14 18:27:42 +0900

    init:React style Theme Setup

---
+++ client/src/styles/theme.ts
@@ -0,0 +1,50 @@
+export const margins = {
+  sm: '',
+  base: '',
+  lg: '',
+  xl: '',
+};
+
+export const paddings = {
+  sm: '',
+  base: '',
+  lg: '',
+  xl: '',
+};
+
+export const fonts = {
+  family: {
+    base: '',
+    title: '',
+  },
+  size: {
+    xs: '12px',
+    sm: '16px',
+    lg: '18px',
+    xl: '24px',
+  },
+  weight: {
+    light: 100,
+    normal: 400,
+    bold: 700,
+  },
+};
+
+export const colors = {
+  primary: '#EE842C',
+  secondary: '#E8B387',
+  error: '#FF322F',
+  alert: '#3FFF3C',
+  white: '#FFFFFF',
+  offWhite: '#FCFCFC',
+  line: '#D7D7D7',
+  placeholder: '#BBBBBB',
+  text: '#222222',
+  gray: '#757575',
+};
+
+export const device = {
+  mobileLength: '575.98px', // 스마트폰 세로
+  mobileWidth: '767.98px', // 스마트폰 가로
+  tablet: '991.98px', // 타블렛
+};
