37f040d1cb0f63938c3222c6b3f6a35683a0076a
park0503 <hooo0503@gmail.com>
2022-11-16 00:51:37 +0900

    feat: nestjs 전역 exception handler 구현 중

--- server/src/AppModule.ts
+++ server/src/AppModule.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { APP_FILTER } from '@nestjs/core';
+import { GlobalExceptionFilter } from './GlobalExceptionFilter';
 import { AuthModule } from './modules/auth';
 
 @Module({
   imports: [AuthModule],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: GlobalExceptionFilter,
+    },
+  ],
 })
 export class AppModule {}
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,16 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './AppModule';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
