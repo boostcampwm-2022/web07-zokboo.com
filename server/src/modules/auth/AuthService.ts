c38b3c334758e7dd474c397d5eb15e49e9070f4c
park0503 <hooo0503@gmail.com>
2022-11-18 07:39:13 +0900

    feat: oauth user 로그인 / 회원가입 service method 구현

--- server/src/modules/auth/AuthService.ts
+++ server/src/modules/auth/AuthService.ts
@@ -1,8 +1,11 @@
 import { ConflictException, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
+import OauthUser from '../user/domain/OauthUser';
 import SigninRequest from '../user/dto/request/SigninRequest';
+import SSOSignupRequest from '../user/dto/request/SSOSigninRequest';
 import SigninResponse from '../user/dto/response/SigninResponse';
+import OauthType from '../user/enum/OauthType';
 import { UserRepository } from '../user/UserRepository';
 import { AuthRepository } from './AuthRepository';
 
@@ -25,7 +28,17 @@ export class AuthService {
     return new SigninResponse(user);
   }
 
-  signupKakao(name: string, email: string, id: number) {
+  async signinByOauth(request: SSOSignupRequest) {
+    const loggedinUser = await this.userRepository.findUserByOauth(request.oauthId, OauthType[request.oauthType]);
+    if (loggedinUser) {
+      return new SigninResponse(loggedinUser);
+    }
+    const newUser = OauthUser.new(OauthType[request.oauthType], request.oauthId);
+    await this.userRepository.save(newUser);
+    return new SigninResponse(newUser);
+  }
+
+  signupKakao(name: string, email: string, id: string) {
     const oauthData = {
       oauth_id: id,
       oauth_type: 'kakao',
