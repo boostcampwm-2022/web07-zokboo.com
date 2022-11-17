eba160875ecedd89c311f9f5c85da591dc63dc57
park0503 <hooo0503@gmail.com>
2022-11-18 06:59:36 +0900

    feat: google passport strategy 구현

---
+++ server/src/modules/auth/strategies/GoogleStrategy.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { Profile, Strategy } from 'passport-google-oauth20';
+
+@Injectable()
+export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
+  constructor(private readonly configService: ConfigService) {
+    super({
+      clientID: configService.get<string>('GOOGLE_APPLICATION_ID'),
+      clientSecret: configService.get<string>('GOOGLE_APPLICATION_SECRET'),
+      callbackURL: configService.get<string>('GOOGLE_CALLBACK_URL'),
+      scope: ['email', 'profile'],
+    });
+  }
+
+  validate(accessToken: string, refreshToken: string, profile: Profile) {
+    const { id, name, emails } = profile;
+
+    return {
+      provider: 'GOOGLE',
+      id,
+      name,
+      email: emails[0].value,
+    };
+  }
+}
