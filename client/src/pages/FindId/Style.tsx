da8ceff9e4cfef609dd6f972ab4259ae2b4eb664
caseBread <kgu0515@gmail.com>
2022-11-22 02:42:24 +0900

    design: 아이디찾기/비밀번호찾기 페이지 레이아웃 설계

---
+++ client/src/pages/FindId/Style.tsx
@@ -0,0 +1,117 @@
+import styled from 'styled-components';
+import { colors, device } from '../../styles/theme';
+
+export const ModalContainer = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+
+  width: 440px;
+
+  border: 1.5px solid #c1c1c1;
+  border-radius: 8px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+
+  background: #fcfcfc;
+
+  padding: 20px;
+
+  @media screen and (max-width: ${device.mobileWidth}) {
+    width: 100%;
+    border: none;
+    box-shadow: none;
+  }
+`;
+
+export const Modal = styled.div`
+  width: 80%;
+`;
+
+export const WhatFindContainer = styled.div`
+  display: flex;
+  align-items: center;
+
+  width: 100%;
+  height: 50px;
+  background: ${colors.gray1};
+
+  padding: 0px;
+  margin: 24px 0px;
+
+  font-size: 14px;
+
+  border: 2px solid ${colors.gray1};
+
+  border-radius: 8px;
+
+  > * {
+    text-decoration: none;
+    color: black;
+    width: 50%;
+    height: 100%;
+    line-height: 50px;
+    border-radius: 8px;
+  }
+
+  .find-id {
+    background: ${colors.white};
+  }
+`;
+export const FindIdContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  width: 100%;
+
+  > * {
+    margin: 4px 0px;
+  }
+`;
+
+export const EmailForm = styled.form`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+
+  width: 100%;
+  height: 100%;
+
+  > input[type='text'] {
+    width: 100%;
+    margin-right: 4px;
+  }
+`;
+
+export const ModalTitle = styled.div``;
+
+export const InputBox = styled.input`
+  padding: 12px;
+
+  border: 1px solid #d7d7d7;
+  border-radius: 8px;
+
+  :focus {
+    outline: none;
+    border: 1px solid ${colors.primary};
+  }
+`;
+
+export const SendButton = styled.input<{ isActived: boolean }>`
+  height: 100%;
+  background: ${(props) => (props.isActived ? colors.primary : colors.secondary)};
+  border: none;
+  border-radius: 8px;
+  padding: 12px;
+  color: ${colors.white};
+
+  :focus {
+    opacity: 0.7;
+  }
+`;
---
+++ client/src/pages/findPw/FindPw.tsx
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import {
+  EmailForm,
+  FindPwContainer,
+  InputBox,
+  Modal,
+  ModalContainer,
+  ModalTitle,
+  SendButton,
+  WhatFindContainer,
+} from './Style';
+
+const FindPw = () => {
+  const [showAuth, setShowAuth] = useState<boolean>(false);
+  const [canAuthSubmit, setCanAuthSubmit] = useState<boolean>(false);
+
+  const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setShowAuth(true);
+  };
+
+  const inputAuthNumberEvent = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.value) {
+      setCanAuthSubmit(true);
+    } else {
+      setCanAuthSubmit(false);
+    }
+  };
+
+  return (
+    <div>
+      <ModalContainer>
+        <Modal>
+          <ModalTitle>비밀번호 찾기</ModalTitle>
+          <WhatFindContainer>
+            <Link className="find-id" to="/find_id">
+              아이디 찾기
+            </Link>
+            <Link className="find-pw" to="/find_pw">
+              비밀번호 찾기
+            </Link>
+          </WhatFindContainer>
+          <FindPwContainer>
+            <InputBox type="text" name="email" placeholder="아이디" required />
+            <EmailForm onSubmit={sendEmail}>
+              <InputBox type="text" name="email" placeholder="이메일" required />
+              <SendButton type="submit" value="인증번호 전송" isActived />
+            </EmailForm>
+            {showAuth && (
+              <InputBox
+                type="text"
+                name="auth-number"
+                placeholder="인증번호"
+                onChange={inputAuthNumberEvent}
+                required
+              />
+            )}
+            {showAuth && <SendButton type="submit" value="확인" isActived={canAuthSubmit} />}
+          </FindPwContainer>
+        </Modal>
+      </ModalContainer>
+    </div>
+  );
+};
+
+export default FindPw;
