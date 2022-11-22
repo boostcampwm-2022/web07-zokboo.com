0088e52398ec37155e600b8c542e073aa72d9a2a
parksang <hooo0503@gmail.com>
2022-11-22 17:31:50 +0900

    test:cicd-test

--- server/src/main.ts
+++ server/src/main.ts
@@ -28,6 +28,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/docs', app, document);
 
-  await app.listen(3000);
+  await app.listen(8080);
 }
 bootstrap();
