ef04692e299511f8c810b7bc89f57e8d812b302a
caseBread <kgu0515@gmail.com>
2022-11-23 16:35:08 +0900

    fix: 검증로직 변수명맞게 boolean으로 수정

--- client/src/hooks/useInput.tsx
+++ client/src/hooks/useInput.tsx
@@ -1,11 +1,11 @@
 import { useState } from 'react';
 
-type Return = [string, (e: React.ChangeEvent<HTMLInputElement>) => void, RegExpMatchArray | null, () => void];
+type Return = [string, (e: React.ChangeEvent<HTMLInputElement>) => void, boolean, () => void];
 
 const useInput = (initialValue: string, verification?: RegExp): Return => {
   const [text, setText] = useState<string>(initialValue);
 
-  const isCorrectInput = verification !== undefined ? text.match(verification) : null;
+  const isCorrectInput = verification !== undefined ? text.match(verification) !== null : true;
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setText(e.target.value);
--- client/src/pages/signup/SignUp.tsx
+++ client/src/pages/signup/SignUp.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import { AiFillEye, AiFillEyeInvisible } from 'react-icons/ai';
-import ButtonComponent from '../../components/common/Button';
 import useInput from '../../hooks/useInput';
 import {
   Button,
@@ -17,6 +16,7 @@ const verification = {
   id: /^(?=.*[a-z])(?=.*[0-9]).{6,16}$/,
   pw: /^(?=.*[a-zA-Z])(?=.*[!@#$%^*=-])(?=.*[0-9]).{8,16}$/,
   email: /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i,
+  certifiedNumber: /.*$/, // 인증번호도 입력값 검증이 필요할까?
 };
 
 const SignUp = () => {
@@ -24,25 +24,48 @@ const SignUp = () => {
   const [pwValue, onPwChange, isPwCorrectInput] = useInput('', verification.pw);
   const [pwCheckValue, onPwCheckChange, isPwCheckCorrectInput] = useInput('', verification.pw);
   const [emailValue, onEmailChange, isEmailCorrectInput] = useInput('', verification.email);
+  const [certifiedNumberValue, onCertifiedNumberChange, isCertifiedNumberCorrectInput] = useInput(
+    '',
+    verification.certifiedNumber,
+  );
   const [visibleInputPw, setVisibleInputPw] = useState<boolean>(false);
   const [visibleInputPwCheck, setVisibleInputPwCheck] = useState<boolean>(false);
+  const [visibleCertifiedNumberInput, setVisibleCertifiedNumberInput] = useState<boolean>(false);
+  const [isCorrectCertifiedNumber, setIsCorrectCertifiedNumber] = useState<boolean>(false);
 
   const handleIsCorrectCheck = {
     id: () => {
-      return isIdCorrectInput !== null || idValue === '';
+      return isIdCorrectInput || idValue === '';
     },
     pw: () => {
-      return isPwCorrectInput !== null || pwValue === '';
+      return isPwCorrectInput || pwValue === '';
     },
     pwCheck: () => {
       return pwValue === pwCheckValue || pwCheckValue === '';
     },
     email: () => {
-      return isEmailCorrectInput !== null || emailValue === '';
+      return isEmailCorrectInput || emailValue === '';
+    },
+    certifiedNumber: () => {
+      return isCertifiedNumberCorrectInput || certifiedNumberValue === '';
     },
   };
 
+  const handleSendEmail = () => {
+    /** 중복이메일 검사 로직 + 인증메일 보내는 로직 */
+
+    alert('인증번호가 발송되었습니다.');
+    setVisibleCertifiedNumberInput(true);
+  };
+
+  const handleCheckCertifiedNumber = () => {
+    /** 인증번호 검증하는 로직 */
+    alert('인증번호가 확인되었습니다.');
+    setIsCorrectCertifiedNumber(true);
+  };
+
   const handleSignup = () => {
+    /** 중복아이디 검사 로직 */
     alert('회원가입이 완료되었습니다.');
     window.location.href = '/login';
   };
@@ -107,19 +130,42 @@ const SignUp = () => {
                 value={emailValue}
                 isCorrect={handleIsCorrectCheck.email()}
               />
+              <Button type="button" value="인증번호 전송" onClick={handleSendEmail} disabled={!isEmailCorrectInput} />
             </InputBoxContainer>
             {handleIsCorrectCheck.email() ? null : <InputAlert>이메일 주소가 올바르지 않습니다.</InputAlert>}
           </InputContainer>
+          {visibleCertifiedNumberInput ? (
+            <InputContainer>
+              <InputTitle>인증번호</InputTitle>
+              <InputBoxContainer>
+                <InputBox
+                  type="text"
+                  placeholder="인증번호"
+                  onChange={onCertifiedNumberChange}
+                  value={certifiedNumberValue}
+                  isCorrect={handleIsCorrectCheck.certifiedNumber()}
+                />
+                <Button
+                  type="button"
+                  value="확인"
+                  onClick={handleCheckCertifiedNumber}
+                  disabled={!isCertifiedNumberCorrectInput}
+                />
+              </InputBoxContainer>
+            </InputContainer>
+          ) : null}
           <Button
             type="button"
             value="회원가입"
             onClick={handleSignup}
             disabled={
-              isIdCorrectInput === null ||
-              isPwCorrectInput === null ||
-              isPwCheckCorrectInput === null ||
-              isEmailCorrectInput === null ||
-              pwValue !== pwCheckValue
+              !isIdCorrectInput ||
+              !isPwCorrectInput ||
+              !isPwCheckCorrectInput ||
+              !isEmailCorrectInput ||
+              !isCertifiedNumberCorrectInput ||
+              !isCorrectCertifiedNumber ||
+              !(pwValue === pwCheckValue)
             }
           />
         </Modal>
