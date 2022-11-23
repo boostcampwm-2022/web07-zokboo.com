511c2b526c839d8fb09437577545c7f359b66b0f
caseBread <kgu0515@gmail.com>
2022-11-23 16:44:34 +0900

    chore : 입력값 최대길이 상수화

--- client/src/hooks/useInput.tsx
+++ client/src/hooks/useInput.tsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 
 type Return = [string, (e: React.ChangeEvent<HTMLInputElement>) => void, boolean, () => void];
 
+const maxTextInput = 30;
+
 const useInput = (initialValue: string, verification?: RegExp): Return => {
   const [text, setText] = useState<string>(initialValue);
 
@@ -9,8 +11,8 @@ const useInput = (initialValue: string, verification?: RegExp): Return => {
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setText(e.target.value);
-    if (e.target.value.length > 30) {
-      setText((prev) => prev.substr(0, 30));
+    if (e.target.value.length > maxTextInput) {
+      setText((prev) => prev.substr(0, maxTextInput));
     }
   };
 
