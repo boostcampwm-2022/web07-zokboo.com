237b3ec3da269463ec393b078c0d11a4b5380c6c
Ing-yeo_Developer <sjw7324@naver.com>
2022-11-24 12:18:53 +0900

    feat:Header Route

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { BiSearchAlt2 } from 'react-icons/bi';
 import { MdArrowDropDown } from 'react-icons/md';
+import { useNavigate } from 'react-router-dom';
+import useToggle from '../../hooks/useToggle';
 import { Button } from '../../styles/common';
 import DropDown from '../common/dropdown/Dropdown';
 import Logo from '../common/Logo';
@@ -18,10 +20,23 @@ import {
   DropDownImage,
   DropDownIcon,
   DropDownContainer,
+  AuthLink,
 } from './Style';
 
 const Header = () => {
-  const [isToggle, setIsToggle] = useState(false);
+  const [isToggle, handleToggle] = useToggle(false);
+  const [input, setInput] = useState('');
+  const navigate = useNavigate();
+
+  const handleSearchWorkbook = () => {
+    navigate(`/search?q=${input}`);
+  };
+
+  const handleSearchToggle = () => {
+    if (isToggle) handleSearchWorkbook();
+    else handleToggle();
+  };
+
   return (
     <Container>
       <InnerContainer>
@@ -31,38 +46,41 @@ const Header = () => {
 
         <ButtonInner>
           <SearchContainer isToggle={isToggle}>
-            <SearchInput placeholder="검색어를 입력하세요." />
-            <SearchButton onClick={() => setIsToggle((prev) => !prev)}>
+            <SearchInput placeholder="검색어를 입력하세요." onChange={(e) => setInput(e.target.value)} />
+            <SearchButton onClick={handleSearchToggle}>
               <BiSearchAlt2 size={30} />
             </SearchButton>
           </SearchContainer>
           <ButtonList>
             {
-              // <>
-              //   <Button>로그인</Button>
-              //   <Button>회원가입</Button>
-              // </>
-              <DropDownContainer>
-                <DropDown
-                  title={
-                    <DropDownSelector>
-                      <DropDownImage />
-                      <DropDownIcon>
-                        <MdArrowDropDown size={30} />
-                      </DropDownIcon>
-                    </DropDownSelector>
-                  }
-                  values={['마이페이지']}
-                  direction="right"
-                />
-              </DropDownContainer>
+              <>
+                <AuthLink to="login">로그인</AuthLink>
+                <AuthLink to="signup">회원가입</AuthLink>
+              </>
+              // <DropDownContainer>
+              //   <DropDown
+              //     title={
+              //       <DropDownSelector>
+              //         <DropDownImage />
+              //         <DropDownIcon>
+              //           <MdArrowDropDown size={30} />
+              //         </DropDownIcon>
+              //       </DropDownSelector>
+              //     }
+              //     values={['마이페이지']}
+              //     direction="right"
+              //   />
+              // </DropDownContainer>
             }
           </ButtonList>
         </ButtonInner>
       </InnerContainer>
 
       <MobileContainer>
-        <SearchInput placeholder="검색어를 입력하세요." />
+        <SearchInput placeholder="검색어를 입력하세요." onChange={(e) => setInput(e.target.value)} />
+        <SearchButton onClick={handleSearchWorkbook}>
+          <BiSearchAlt2 size={30} />
+        </SearchButton>
       </MobileContainer>
     </Container>
   );
