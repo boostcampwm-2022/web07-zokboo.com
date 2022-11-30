b9a7a9832e6fc0b66686949d21299fda364720d8
caseBread <kgu0515@gmail.com>
2022-11-30 21:35:41 +0900

    chore: searchResultItem style 분리

--- client/src/components/search/SearchResultItem/SearchResultItem.tsx
+++ client/src/components/search/SearchResultItem/SearchResultItem.tsx
@@ -1,81 +1,20 @@
-import styled from 'styled-components';
 import { FiMoreHorizontal } from 'react-icons/fi';
 import { AiOutlineHeart, AiFillHeart } from 'react-icons/ai';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { colors, device } from '../../styles/theme';
-
-const SearchResultItemContainer = styled.div`
-  border: 1px solid ${colors.gray3};
-  border-radius: 4px;
-  box-shadow: 2px 2px ${colors.gray1};
-  padding: 16px 12px;
-  margin: 20px 0px;
-  .more-button {
-    float: right;
-  }
-  @media screen and (max-width: ${device.mobileWidth}) {
-    border: none;
-    border-radius: 0px;
-    border-bottom: 1px solid ${colors.gray3};
-    box-shadow: none;
-  }
-`;
-
-const ItemTitle = styled.div``;
-const ItemExplain = styled.div`
-  font-size: 12px;
-  color: ${colors.gray4};
-  padding: 8px 0px;
-`;
-const ItemCreator = styled.div`
-  font-size: 8px;
-  color: ${colors.gray3};
-`;
-
-const ItemCreateAt = styled.div`
-  font-size: 8px;
-  color: ${colors.gray3};
-`;
-
-const Buttons = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: flex-end;
-  margin-top: 16px;
-`;
-
-const ItemInfo = styled.div`
-  display: flex;
-  align-items: flex-end;
-  font-size: 14px;
-  color: ${colors.gray4};
-  > * {
-    padding: 4px;
-  }
-  .fillStyled {
-    color: ${colors.primary};
-  }
-`;
-
-const Heart = styled.div``;
-
-const ContentButtons = styled.div`
-  float: right;
-  > * {
-    margin: 0px 0px 0px 24px;
-    padding: 4px;
-    background: none;
-    border: 1px solid ${colors.primary};
-    border-radius: 4px;
-    color: ${colors.primary};
-    :hover {
-      opacity: 0.7;
-    }
-  }
-`;
-const TestButton = styled.input``;
-const SaveButton = styled.input``;
+import {
+  Buttons,
+  ContentButtons,
+  Heart,
+  ItemCreateAt,
+  ItemCreator,
+  ItemExplain,
+  ItemInfo,
+  ItemTitle,
+  SaveButton,
+  SearchResultItemContainer,
+  TestButton,
+} from './Style';
 
 interface SearchResultItemProps {
   id: number;
