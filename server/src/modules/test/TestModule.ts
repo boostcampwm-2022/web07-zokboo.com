014af4e5905095b735cef311102c17e120fc9b5b
park0503 <hooo0503@gmail.com>
2022-12-01 00:36:58 +0900

    feat: 시험 생성 query 메소드 구현

--- server/src/modules/test/TestModule.ts
+++ server/src/modules/test/TestModule.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { CommonModule } from '../common/CommonModule';
+import { TestRepository } from './TestRepository';
 
 @Module({
   imports: [CommonModule],
+  providers: [TestRepository],
 })
 export class TestModule {}
