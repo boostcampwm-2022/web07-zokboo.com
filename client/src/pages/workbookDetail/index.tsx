bf3cc0dc21faa1e17239d4339121159a0da25655
caseBread <kgu0515@gmail.com>
2022-12-07 21:56:44 +0900

    chore : 안쓰는 함수 정리 및 style 분리

--- client/src/pages/workbookDetail/index.tsx
+++ client/src/pages/workbookDetail/index.tsx
@@ -1,199 +1,36 @@
-import axios, { AxiosError } from 'axios';
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useState } from 'react';
 import { useQuery } from 'react-query';
 import { useSearchParams } from 'react-router-dom';
-import styled from 'styled-components';
 import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai';
 import { getWorkbookById } from '../../api/workbook';
-import ButtonComponent from '../../components/common/Button';
-import { colors, fonts, media, widths } from '../../styles/theme';
-import { SERVER_URL } from '../../utils/constants';
 import SampleImage from '../../images/sample-image.jpeg';
-import DropDown from '../../components/common/dropdown/Dropdown';
 import SampleQuestionImage from '../../images/sample-question-image.png';
 
-const PageContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-
-  width: 100%;
-
-  height: 100%;
-`;
-
-const HeaderContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-
-  justify-content: center;
-  ${media.mobileLength} {
-    justify-content: flex-start;
-  }
-
-  gap: 8px;
-
-  background-color: ${colors.secondary};
-  padding: 20px 0;
-`;
-
-const Header = styled.div`
-  display: flex;
-  justify-content: flex-start;
-`;
-
-const Left = styled.div`
-  display: flex;
-  ${media.mobileWidth} {
-    display: none;
-  }
-  flex-direction: row;
-  align-items: center;
-
-  gap: 4px;
-  padding-left: 20px;
-`;
-const Right = styled.div`
-  padding-left: 20px;
-  width: 100%;
-  max-width: 400px;
-`;
-
-const WorkbookIntroduce = styled.div`
-  margin-bottom: 150px;
-`;
-const ButtonContainer = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-`;
-const WorkbookSaveButton = styled.input`
-  padding: 10px 20px;
-
-  border: 1.5px solid ${colors.primary};
-  border-radius: 8px;
-
-  :hover {
-    opacity: 0.8;
-  }
-
-  :active {
-    opacity: 0.5;
-  }
-`;
-
-const Heart = styled.button`
-  background: none;
-  border: none;
-`;
-
-const Title = styled.div`
-  font-size: 26px;
-  font-weight: 700;
-  letter-spacing: -0.3px;
-  line-height: 1.5;
-`;
-const IsPublic = styled.div`
-  display: inline-block;
-
-  background-color: ${colors.primary};
-  color: ${colors.white};
-
-  border: none;
-  border-radius: 4px;
-  font-size: ${fonts.size.xs};
-  font-weight: 500;
-  padding: 2px 4px;
-`;
-const Description = styled.div``;
-
-const BodyTitle = styled.div`
-  font-weight: ${fonts.weight.bold};
-  font-size: ${fonts.size.xl};
-
-  margin-top: 20px;
-`;
-
-const ProblemListContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  ${media.mobileWidth} {
-    justify-content: flex-start;
-  }
-
-  margin: 0 10px;
-`;
-const ProblemList = styled.div`
-  width: 70%;
-  ${media.mobileWidth} {
-    width: 100%;
-  }
-`;
-
-const ProblemDropdown = styled.details`
-  margin-top: 12px;
-
-  > * {
-    padding: 10px 20px;
-  }
-`;
-
-const ProblemNumber = styled.summary`
-  white-space: nowrap;
-  overflow: hidden;
-
-  background-color: ${colors.secondary};
-  border-radius: 4px;
-  margin-bottom: 8px;
-
-  ::marker {
-    display: none;
-    content: '🔽  ';
-  }
-`;
-
-const Problem = styled.div`
-  background-color: white;
-`;
-
-const ProblemTitle = styled.div`
-  font-weight: 700;
-`;
-
-const ProblemImg = styled.img`
-  max-width: 350px;
-  max-height: 350px;
-`;
-
-const ProblemDifficulty = styled.div`
-  color: ${colors.gray4};
-  font-size: ${fonts.size.xs};
-`;
-const ProblemCommentary = styled.div`
-  color: ${colors.gray4};
-  font-size: ${fonts.size.xs};
-`;
-const ProblemHashtags = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-
-  margin-top: 8px;
-
-  font-size: 14px;
-
-  > * {
-    font-size: 12px;
-
-    border: none;
-    border-radius: 4px;
-
-    margin: 0 4px;
-    padding: 2px 4px;
-
-    background-color: ${colors.primary};
-    color: ${colors.white};
-  }
-`;
+import {
+  BodyTitle,
+  ButtonContainer,
+  Description,
+  Header,
+  HeaderContainer,
+  Heart,
+  IsPublic,
+  Left,
+  PageContainer,
+  Problem,
+  ProblemCommentary,
+  ProblemDifficulty,
+  ProblemDropdown,
+  ProblemHashtags,
+  ProblemImg,
+  ProblemList,
+  ProblemListContainer,
+  ProblemNumber,
+  ProblemTitle,
+  Right,
+  Title,
+  WorkbookIntroduce,
+  WorkbookSaveButton,
+} from './Style';
 
 interface Question {
   questionId: number;
@@ -213,10 +50,6 @@ interface Workbook {
   description: string;
   isPublic: boolean;
   questions: Question[];
-}
-interface WorkbookResponse {
-  msg: string;
-  data: Workbook[] | Workbook;
 }
 
 const WorkbookDetail = () => {
