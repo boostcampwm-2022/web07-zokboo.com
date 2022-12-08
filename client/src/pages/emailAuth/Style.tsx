8afec35336ef76df75fdf1db81c56731566a140c
Ing-yeo_Developer <sjw7324@naver.com>
2022-12-08 16:15:10 +0900

    refactor:email auth

--- client/src/pages/emailAuth/Style.tsx
+++ client/src/pages/emailAuth/Style.tsx
@@ -1,15 +1,7 @@
 import styled from 'styled-components';
 import { colors, fonts } from '../../styles/theme';
 
-export const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-
-  width: 100%;
-  height: 100vh;
-`;
+export const Container = styled.div``;
 
 export const Title = styled.h1`
   margin: 0;
@@ -18,6 +10,22 @@ export const Title = styled.h1`
   strong {
     color: ${colors.primary};
   }
+`;
+
+export const Contents = styled.p`
+  margin: 5px 0 0;
+  font-size: ${fonts.size.lg};
+`;
+
+export const Inner = styled.div<{ isShow: boolean }>`
+  display: ${(props) => (props.isShow ? `flex` : `none`)};
+
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  width: 100%;
+  height: 100vh;
 `;
 
 export const LoadingContainer = styled.div`
--- client/src/pages/emailAuth/index.tsx
+++ client/src/pages/emailAuth/index.tsx
@@ -1,24 +1,36 @@
 import { AxiosError } from 'axios';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useQuery } from 'react-query';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import checkEmailAuth from '../../api/auth';
 import Loading from '../../components/common/Loading';
 import KEYS from '../../react-query/keys/auth';
-import { Container, LoadingContainer, Title } from './Style';
+import { Container, Contents, Inner, LoadingContainer, Title } from './Style';
 
 const EmailAuth = () => {
   const [searchParams, _] = useSearchParams();
   const navigate = useNavigate();
 
+  const [count, setCount] = useState(3);
+
   const token = searchParams.get('token');
 
-  useQuery([KEYS.email, token], checkEmailAuth, {
+  const { isSuccess } = useQuery([KEYS.email, token], checkEmailAuth, {
     enabled: token !== null,
     onSuccess: () => {
       toast.success('이메일 인증이 완료되었습니다.');
-      navigate('/');
+      // navigate('/');
+      let cnt = 0;
+      const interval = setInterval(() => {
+        setCount((prev) => prev - 1);
+        cnt += 1;
+
+        if (cnt === 3) {
+          clearInterval(interval);
+          navigate('/');
+        }
+      }, 1000);
     },
     onError: (error: AxiosError) => {
       toast.error(error.message);
@@ -35,12 +47,22 @@ const EmailAuth = () => {
 
   return (
     <Container>
-      <Title>
-        이메일 <strong>인증</strong> 확인 중 입니다.
-      </Title>
-      <LoadingContainer>
-        <Loading />
-      </LoadingContainer>
+      <Inner isShow={!isSuccess}>
+        <Title>
+          이메일 <strong>인증</strong> 확인 중 입니다.
+        </Title>
+
+        <LoadingContainer>
+          <Loading />
+        </LoadingContainer>
+      </Inner>
+
+      <Inner isShow={isSuccess}>
+        <Title>
+          계정 <strong>인증</strong>이 완료되었습니다.
+        </Title>
+        <Contents>{count}초 뒤 메인 페이지로 이동합니다.</Contents>
+      </Inner>
     </Container>
   );
 };
