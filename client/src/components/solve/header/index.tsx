8ebcdae5be32990a1fa21d043114aef46546cf3d
Ing-yeo_Developer <sjw7324@naver.com>
2022-12-11 19:11:07 +0900

    save

--- client/src/components/solve/header/index.tsx
+++ client/src/components/solve/header/index.tsx
@@ -1,17 +1,20 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useAppSelector } from '../../../redux/hooks';
 import selectSolveData from '../../../redux/solve/selector';
+import TYPE from '../../../types/solve';
 import Logo from '../../common/logo';
 import { Container, Inner, LogoBox, Title } from './Style';
 
 const Header = () => {
   const { title, minutes, seconds, createdAt, type } = useAppSelector(selectSolveData);
-
+  const [timmer, setTimmer] = useState(0);
   console.log(minutes, seconds, createdAt);
 
-  // useEffect(() => {
-  //   if(type === "")
-  // },[type])
+  useEffect(() => {
+    if (type === TYPE.test) {
+      console.log(minutes);
+    }
+  }, [type]);
 
   return (
     <Container>
